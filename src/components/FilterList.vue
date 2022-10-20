d8c09bda1e313f8aec6d4dc5f79af33409450e01
mansidhamelia <mansidhamelia6@gmail.com>
2022-10-20 23:52:27 +0530

    fix: minor changes

--- src/components/FilterList.vue
+++ src/components/FilterList.vue
@@ -1,21 +1,95 @@
 <template>
-  <h2>Search List</h2>
-  <div>
-    <input
-      v-model="search"
-      type="text"
-      id="Input"
-      placeholder="Search here...."
-    />
-  </div>
-  <div>
-    <ul id="myList">
-      <li v-for="data in result">{{ data.item }}</li>
-    </ul>
+  <div class="">
+    <h2>Search List</h2>
+    <div>
+      <input
+        v-model="search"
+        type="text"
+        id="Input"
+        placeholder="Search here...."
+        v-on:keyup.enter="save(search)"
+      />
+    </div>
+
+    <div>
+      <ul id="myList">
+        <li v-for="(data, i) in result" :key="i">
+          {{ data.item }} <a id="edit" @click="edit(data)"> Edit</a> 
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      search: "",
+      isEditing: false,
+      currentId: "",
+      resources: [
+        { item: "Alpha", id: 1 },
+        { item: "Applet", id: 2 },
+        { item: "Aim", id: 3 },
+        { item: "Bite", id: 4 },
+        { item: "ByteFury", id: 5 },
+        { item: "Cindy", id: 6 },
+        { item: "Chemistry", id: 7 },
+        { item: "Dog", id: 8 },
+        { item: "Eye", id: 9 },
+        { item: "Elephant", id: 10 },
+      ],
+    };
+  },
+  computed: {
+    result() {
+      if (this.search) {
+        return this.resources.filter((data) => {
+          return this.search
+            .toLowerCase()
+            .split(" ")
+            .every((v) => data.item.toLowerCase().includes(v));
+        });
+      } else {
+        return this.resources;
+      }
+    },
+  },
+  methods: {
+    edit(data) {
+      this.isEditing = true
+      this.search = data.item;
+      this.currentId = data.id;
+    },
+
+    save(data) {
+      const pos = this.resources.findIndex((r) => r.id == this.currentId);
+      this.resources[pos].item = data;
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+
 <style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
 #Input {
   width: 70%;
   font-size: 16px;
@@ -47,5 +121,10 @@ input:hover {
 }
 #myList li:hover {
   background-color: rgb(149, 149, 149);
+}
+
+#edit {
+  padding-left: 30px;
+cursor: pointer;
 }
 </style>
